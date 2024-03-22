4a867f3d114e5f667f9877b491f014d0cb321807
VLstack <github@vlstack.fr>
2024-03-22 11:27:06 +0100

    first implement

--- Sources/VLBundleKit/VLBundleKit.swift
+++ Sources/VLBundleKit/VLBundleKit.swift
@@ -1,2 +1,29 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+
+public extension Bundle
+{
+ var releaseVersionNumber: String
+ {
+  guard let version: String = infoDictionary?["CFBundleShortVersionString"] as? String
+  else { return "?" }
+  
+  return version
+ }
+ 
+ var buildVersionNumber: String
+ {
+  guard let version: String = infoDictionary?["CFBundleVersion"] as? String
+  else { return "?" }
+  
+  return version
+ }
+ 
+ var fullVersionNumber: String { fullVersionNumber(format: nil) }
+ 
+ func fullVersionNumber(format: String? = nil) -> String
+ {
+  let format: String = format ?? "%@ (%@)"
+  
+  return String(format: format, releaseVersionNumber, buildVersionNumber)
+ }
+}
