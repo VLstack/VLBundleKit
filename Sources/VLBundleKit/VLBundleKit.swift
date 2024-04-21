6f96edbce982ec7a79bd8358eec32aca31727f0c
VLstack <github@vlstack.fr>
2024-04-21 10:24:42 +0200

    add property "bundleIdentifierVersioned"

--- Sources/VLBundleKit/VLBundleKit.swift
+++ Sources/VLBundleKit/VLBundleKit.swift
@@ -2,14 +2,6 @@ import Foundation
 
 public extension Bundle
 {
- var releaseVersionNumber: String
- {
-  guard let version: String = infoDictionary?["CFBundleShortVersionString"] as? String
-  else { return "?" }
-  
-  return version
- }
- 
  var buildVersionNumber: String
  {
   guard let version: String = infoDictionary?["CFBundleVersion"] as? String
@@ -18,10 +10,26 @@ public extension Bundle
   return version
  }
  
+ var bundleIdentifierVersioned: String
+ {
+  let id: String = Bundle.main.bundleIdentifier ?? "No bundle identifier"
+  let version: String = fullVersionNumber
+  
+  return "\(id) v.\(version)"
+ }
+
  var fullVersionNumber: String { fullVersionNumber() }
  
  func fullVersionNumber(format: String = "%@ (%@)") -> String
  {
   return String(format: format, releaseVersionNumber, buildVersionNumber)
+ }
+ 
+ var releaseVersionNumber: String
+ {
+  guard let version: String = infoDictionary?["CFBundleShortVersionString"] as? String
+  else { return "?" }
+  
+  return version
  }
 }
